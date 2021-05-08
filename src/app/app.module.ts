7a60a62e8f47067e8024c1b5016b89987379a839
shiva-sms <shiva.thatishetti123@gmail.com>
2021-05-08 20:38:42 +0530

    nothing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,6 @@ import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
 import { UserListComponent } from './user-list/user-list.component'
 import {UserRegistrationComponent} from './user-registration/user-registration.component';
-import { CardsComponent } from './cards/cards.component';
-import { Cards2Component } from './cards2/cards2.component';
-import { Cards3Component } from './cards3/cards3.component';
-import { Cards4Component } from './cards4/cards4.component';
-import { Cards5Component } from './cards5/cards5.component';
-import { Cards6Component } from './cards6/cards6.component';
 import { ProductsComponent } from './products/products.component';
 import { FooterComponent } from './footer/footer.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
@@ -24,12 +18,6 @@ import { ContactComponent } from './contact/contact.component'
     AppComponent,
     UserListComponent,
     UserRegistrationComponent,
-    CardsComponent,
-    Cards2Component,
-    Cards3Component,
-    Cards4Component,
-    Cards5Component,
-    Cards6Component,
     ProductsComponent,
     FooterComponent,
     ProductDetailsComponent,
