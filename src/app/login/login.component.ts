93586fd45af968da51ad453e33a0f9056ed273e5
shiva-sms <shiva.thatishetti123@gmail.com>
2021-05-09 13:08:25 +0530

    Assignment 22

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,36 +7,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent {
 
-  users=[];
-  uobj:user={uname:'',dob:'',cont:'',mail:'',password:''}
-  //list of user array
-  addUserData()
-  {
-    let obj=this.uobj
-    console.log(obj)
-    //this.users.push(uobj);
-    //ref.reset();
-    this.uobj={uname:'',dob:'',cont:'',mail:'',password:''}
-  }
-  //deleteing username
-    deleteUser(index)
-  {
-    
-    this.users.splice(index,1)
-  }
-
-
+  
 }
 
-
-
-
-
-class user
-{
-  uname:string
-  dob:string
-  cont:string
-  mail:string
-  password:string
-}
